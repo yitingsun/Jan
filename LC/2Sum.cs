12544319fe9d550a599570bca8163f47dec896e7
Yiting <yitingsun97@gmail.com>
2023-02-02 13:44:51 -0500

    LC questions

---
+++ LC/2Sum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class TwoSum
+{
+    public static int[] FindTwoSum(int[] nums, int target)
+    {
+        var map = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            if (map.ContainsKey(complement))
+            {
+                return new int[] { map[complement], i };
+            }
+            map[nums[i]] = i;
+        }
+        throw new ArgumentException("No two sum solution");
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] nums = new int[] { 2, 7, 11, 15 };
+        int target = 9;
+        int[] result = FindTwoSum(nums, target);
+        Console.WriteLine($"{result[0]}, {result[1]}");
+    }
+}
+
---
+++ LC/3Sum.cs
@@ -0,0 +1,44 @@
+﻿class ThreeSum
+{
+    public static IList<IList<int>> ThreeSumSolutions(int[] nums)
+    {
+        Array.Sort(nums);
+        IList<IList<int>> res = new List<IList<int>>();
+        for (int i = 0; i < nums.Length - 2; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int l = i + 1, r = nums.Length - 1;
+            while (l < r)
+            {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum == 0)
+                {
+                    res.Add(new List<int> { nums[i], nums[l], nums[r] });
+                    while (l < r && nums[l] == nums[l + 1]) l++;
+                    while (l < r && nums[r] == nums[r - 1]) r--;
+                    l++;
+                    r--;
+                }
+                else if (sum < 0)
+                {
+                    l++;
+                }
+                else
+                {
+                    r--;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] nums = new int[] { -1, 0, 1, 2, -1, -4 };
+        IList<IList<int>> res = ThreeSumSolutions(nums);
+        foreach (var solution in res)
+        {
+            Console.WriteLine(string.Join(", ", solution));
+        }
+    }
+}
